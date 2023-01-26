b834bdc4822a8c96bfb19e940da652c68ccd381e
B#rg#ndy1 <tc78200@gmail.com>
2023-01-26 22:48:25 +0100

    ajout logo header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import { useState } from "react";
 import Form from "./components/Form";
 import Header from "./components/Header";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrash, faListAlt } from "@fortawesome/free-solid-svg-icons";
+import { faListAlt as farListAlt } from "@fortawesome/free-regular-svg-icons";
+library.add(faTrash, faListAlt, farListAlt);
+
 function App() {
   const [todoList, setToDoList] = useState([]);
 
